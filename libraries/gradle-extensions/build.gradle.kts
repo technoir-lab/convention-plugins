2d88bd467bff7b8102f5e1fb21c0824672b32e37
technoir <technoir.dev@gmail.com>
2025-11-05 18:53:23 +0000

    Enable ABI validation in gradle-extensions library

--- libraries/gradle-extensions/build.gradle.kts
+++ libraries/gradle-extensions/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("io.technoirlab.conventions.jvm-library")
 }
 
+jvmLibrary {
+    buildFeatures {
+        abiValidation = true
+    }
+}
+
 dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
