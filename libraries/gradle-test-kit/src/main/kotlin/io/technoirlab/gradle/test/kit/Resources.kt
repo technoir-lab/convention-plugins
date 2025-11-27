1cb4f5950009d338b98b1cf2edccf01d70de4e52
Sergey C. <technoir.dev@gmail.com>
2025-11-27 23:36:48 +0000

    Switch functional tests to publishing plugins to local Maven repository (#97)
    * Switch functional tests to publishing plugins to local Maven repository
    
    * Remove no longer necessary build step

--- libraries/gradle-test-kit/src/main/kotlin/io/technoirlab/gradle/test/kit/Resources.kt
+++ libraries/gradle-test-kit/src/main/kotlin/io/technoirlab/gradle/test/kit/Resources.kt
@@ -2,11 +2,12 @@ package io.technoirlab.gradle.test.kit
 
 import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.copyToRecursively
 
-@OptIn(ExperimentalPathApi::class)
 internal fun copyResources(dirName: String, targetDir: Path) {
     val resourcesDir = Paths.get(::copyResources.javaClass.classLoader.getResource(dirName)!!.toURI())
     resourcesDir.copyToRecursively(targetDir, overwrite = false, followLinks = true)
 }
+
+internal fun readTextResource(fileName: String): String =
+    ::readTextResource.javaClass.classLoader.getResource(fileName)!!.readText()
