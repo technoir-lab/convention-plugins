b978f889c5473d3f7ea6b9ea813a19a7fe15ffb6
technoir <technoir.dev@gmail.com>
2026-01-03 22:19:25 +0800

    Add a functional test for Maven Central publishing

--- root-conventions/src/functionalTest/kotlin/io/technoirlab/conventions/root/RootConventionPluginTest.kt
+++ root-conventions/src/functionalTest/kotlin/io/technoirlab/conventions/root/RootConventionPluginTest.kt
@@ -3,6 +3,7 @@ package io.technoirlab.conventions.root
 import io.technoirlab.gradle.test.kit.GradleRunnerExtension
 import io.technoirlab.gradle.test.kit.appendBuildScript
 import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
@@ -53,5 +54,20 @@ class RootConventionPluginTest {
         val buildResult = gradleRunner.build(":dokkaGenerate")
 
         assertThat(buildResult.task(":jvm-library:dokkaGenerateModuleHtml")?.outcome).isNotNull
+    }
+
+    @Test
+    fun `Maven Central publishing`() {
+        gradleRunner.root.appendBuildScript(
+            """
+            dependencies {
+                nmcpAggregation(project(":jvm-library"))
+            }
+            """.trimIndent()
+        )
+
+        val buildResult = gradleRunner.build(":nmcpZipAggregation")
+
+        assertThat(buildResult.task(":nmcpZipAggregation")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
     }
 }
--- root-conventions/src/functionalTest/resources/sample-project/jvm-library/build.gradle.kts
+++ root-conventions/src/functionalTest/resources/sample-project/jvm-library/build.gradle.kts
@@ -1,9 +1,21 @@
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
     `java-library`
     `jvm-test-suite`
+    `maven-publish`
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.dokka")
     id("org.jetbrains.kotlinx.kover")
+    id("com.gradleup.nmcp")
+}
+
+publishing {
+    publications {
+        val libraryMaven by registering(MavenPublication::class) {
+            from(components["java"])
+        }
+    }
 }
 
 testing {
