a4a333ccb56c4fd6587b11a1383785d4fc7cf544
Sergey C. <technoir.dev@gmail.com>
2026-06-05 18:55:01 +0100

    Bump conventionPluginsVersion to v50 (#209)

--- libraries/gradle-test-kit/src/main/kotlin/io/technoirlab/gradle/test/kit/Resources.kt
+++ libraries/gradle-test-kit/src/main/kotlin/io/technoirlab/gradle/test/kit/Resources.kt
@@ -2,8 +2,10 @@ package io.technoirlab.gradle.test.kit
 
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.copyToRecursively
 
+@OptIn(ExperimentalPathApi::class)
 internal fun copyResources(dirName: String, targetDir: Path) {
     val resourcesDir = Paths.get(::copyResources.javaClass.classLoader.getResource(dirName)!!.toURI())
     resourcesDir.copyToRecursively(targetDir, overwrite = false, followLinks = true)
