88e42794e70ac958356cbaa48ab0099cb73f9d39
technoir <technoir.dev@gmail.com>
2026-08-22 19:18:46 +0100

    Filter-out optional checksums from Maven Central publications

--- conventions/settings-conventions/src/main/kotlin/io/technoirlab/conventions/settings/configuration/Publishing.kt
+++ conventions/settings-conventions/src/main/kotlin/io/technoirlab/conventions/settings/configuration/Publishing.kt
@@ -4,7 +4,9 @@ import nmcp.NmcpAggregationExtension
 import nmcp.NmcpSettings
 import org.gradle.api.Project
 import org.gradle.api.initialization.Settings
+import org.gradle.api.tasks.bundling.Zip
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
 
 internal fun Settings.configurePublishing() {
     // TODO: Remove .orNull when Nmcp bug is fixed
@@ -24,5 +26,12 @@ internal fun Project.configurePublishing() {
         extensions.configure<NmcpAggregationExtension> {
             allowDuplicateProjectNames.set(true)
         }
+
+        tasks.withType<Zip>()
+            .matching { it.name.startsWith("nmcpZip") }
+            .configureEach {
+                // Maven Central requires MD5 and SHA-1; SHA-512 is optional and increases the published file count.
+                exclude("**/*.sha512")
+            }
     }
 }
