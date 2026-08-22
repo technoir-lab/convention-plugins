b5f069021307571617bb9b1467f6eac62e09770e
Sergey C. <technoir.dev@gmail.com>
2026-08-22 19:50:14 +0100

    Bump conventionPluginsVersion to v55 (#269)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         mavenLocal()
     }
     plugins {
-        val conventionPluginsVersion = "v54"
+        val conventionPluginsVersion = "v55"
         id("io.technoirlab.conventions.gradle-plugin") version conventionPluginsVersion
         id("io.technoirlab.conventions.jvm-application") version conventionPluginsVersion
         id("io.technoirlab.conventions.jvm-library") version conventionPluginsVersion
